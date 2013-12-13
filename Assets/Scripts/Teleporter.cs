7fef49cb07579c728c3a39f3533aa7b2555635ab
zackLore <zacktron@gmail.com>
2013-12-12 21:11:51 -0800

    Added teleport blocks

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+	public PlayerControl playerControlRef;
+	public static float nextTeleport;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		//This only works if there is one of each block in the level... got it to work but will need to be modified to handle more than 2 teleport objects
+		if(nextTeleport > Time.time)
+		{
+			Debug.Log(Time.time);
+			return;
+		}
+		
+		try
+		{
+			if(this.gameObject.name.EndsWith("1"))
+			{
+				playerControlRef.transform.position = GameObject.Find("TeleportBlock2").transform.position;
+				nextTeleport = Time.time + 2;
+				Debug.Log ("Teleport! " + nextTeleport);			
+			}
+			else
+			{
+				playerControlRef.transform.position = GameObject.Find("TeleportBlock1").transform.position;
+				nextTeleport = Time.time + 2;
+				Debug.Log ("Teleport! " + nextTeleport);
+			}
+		}
+		catch(UnityException ex)
+		{
+			Debug.Log ("Error: " + ex.Message);
+		}	
+	}	
+}
