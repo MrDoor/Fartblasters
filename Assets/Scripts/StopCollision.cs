b780321fb6ced2c306d11274228f6c92e6e9d04d
Bryce <mrdoor@gmail.com>
2013-12-13 22:39:36 -0800

    Cleaned up StopCollision. Made a small non-zero float to be used everywhere, added a comment about why.

--- Assets/Scripts/StopCollision.cs
+++ Assets/Scripts/StopCollision.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class StopCollision : MonoBehaviour 
 {
 	void OnCollisionEnter2D( Collision2D coll )
-	{		
+	{	
+		// Don't set it to zero, want it to keep moving just a little
+		float newVelocity = 0.005f;
 		try
 		{
-			coll.transform.rigidbody2D.velocity = new Vector2( 0.005f, 0.005f );
-			coll.transform.rigidbody2D.angularVelocity = 0.005f;
+			coll.transform.rigidbody2D.velocity = new Vector2( newVelocity, newVelocity );
+			coll.transform.rigidbody2D.angularVelocity = newVelocity;
 		}
 		catch(UnityException ex)
 		{
