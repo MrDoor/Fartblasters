0ff2550915314524234395a027e67becc887d6ad
Bryce <mrdoor@gmail.com>
2013-12-13 22:38:31 -0800

    Added Stop Blocks Created Stop Block prefab, added StopCollision script. Moved green teleport block up Put slippery blocks in the bottom right corner Replaced the old slippery blocks with default blocks Added a stop block in the middle of the new slippery blocks Added stop blocks to the top Added a couple more picks to entice people to try the new blocks Renamed all the controllers to containers, as that makes more sense Reorganized the blocks in the hierarchy Added layer tags for all the new block prefab types Fixed the sort layer of the teleport block prefabs

---
+++ Assets/Scripts/StopCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopCollision : MonoBehaviour 
+{
+	void OnCollisionEnter2D( Collision2D coll )
+	{		
+		try
+		{
+			coll.transform.rigidbody2D.velocity = new Vector2( 0.005f, 0.005f );
+			coll.transform.rigidbody2D.angularVelocity = 0.005f;
+		}
+		catch(UnityException ex)
+		{
+			Debug.Log("Error: " + ex.Message);
+		}
+	}
+}
