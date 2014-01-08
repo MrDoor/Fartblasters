232a642bfe2f5069fc1e6cd5b69c28ab4ba50d9e
zackLore <zacktron@gmail.com>
2014-01-07 22:26:53 -0800

    Fixed FallingBlock script to use coroutines.  Added an AudioHandler object that we can use as a single audio source for farts n' such.

---
+++ Assets/Scripts/AudioHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioHandler : MonoBehaviour {
+
+	public AudioClip[] farts = new AudioClip[12];
+	// Use this for initialization
+	void Start () {
+
+		this.audio.clip = farts[Random.Range(0, farts.Length)];
+		Debug.Log ("Playing Test Fart");
+		this.audio.Play();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/FallingBlockCollision.cs
+++ Assets/Scripts/FallingBlockCollision.cs
@@ -3,101 +3,62 @@ using System.Collections;
 
 public class FallingBlockCollision : MonoBehaviour {
 	
-	float fallTime = 0.0f;
-	float nextFlash = 0.0f;
-	float killTime = 0.0f;
+	public float fallTime 		= 2.5f;
+	public float totalBlinkTime_= 1.0f;
 	
-	const float FLASH_TIME = 0.33f;
-	bool flashFlag = false;
-	bool canFall = false;	
-	bool visibleFlag = false;
+	private float blinkTime 	= 0.1f;	
 	
 	// Use this for initialization
 	void Start () {		
-		//Debug.Log("Start:gravity = " + this.transform.rigidbody2D.gravityScale);
-		//this.transform.rigidbody2D.gravityScale = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(nextFlash > 0)
-		{
-			if(Time.time >= nextFlash)
-			{
-				if(flashFlag)
-				{
-					flashFlag = false;
-				}
-				else
-				{
-					flashFlag = true;
-				}
-				
-				this.transform.renderer.enabled = flashFlag;
-				nextFlash = Time.time + FLASH_TIME;
-			}
-		}
-		
-		if(canFall && Time.time >= fallTime)
-		{		
-			Debug.Log ("Begin Fall - fallTime: " + fallTime + " - Time.time: " + Time.time);
-			//GameObject gObject = new GameObject();
-			if(this.transform.rigidbody2D == null)
-			{
-				Rigidbody2D rigidBody = this.gameObject.AddComponent<Rigidbody2D>();
-				this.transform.rigidbody2D.gravityScale = 1;
-				this.transform.rigidbody2D.mass = 10;
-				nextFlash = 0;
-				this.transform.renderer.enabled = true;
-				//canFall = false;				
-			}
-			
-			if(killTime > 0)
-			{
-				if(visibleFlag)
-				{
-					visibleFlag = false;
-				}
-				else
-				{
-					visibleFlag = true;
-				}
-				this.transform.renderer.enabled = visibleFlag;
-				
-				if(Time.time >= killTime)
-				{					
-					Destroy(this.gameObject);
-				}
-			}
-			//this.transform.rigidbody2D.isKinematic = false;
-			//this.transform.rigidbody2D.gravityScale = 1;
-			//this.transform.rigidbody2D.mass += 1;
-		}		
+	
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll)
 	{
-		if(coll.gameObject.name.Equals("Player"))
+		if(coll.gameObject.tag.Equals("Player"))
 		{
-			PlayerControl pControl = (PlayerControl)coll.gameObject.GetComponent<PlayerControl>();
-			if(pControl)
-			{
-				if(fallTime == 0.0f)
-				{
-					fallTime = Time.time + 3.0f;
-					nextFlash = Time.time + FLASH_TIME;
-					//this.transform.rigidbody2D.isKinematic = true;
-					canFall = true;
-				}
-			}
+			StartCoroutine(Fall ());			
 		}
 		else
 		{	
-			if(killTime <= 0)
-			{
-				killTime = Time.time + 1f;
-			}
+			StartCoroutine(FadeOut());
 		}	
-	}		
+	}	
+	
+	IEnumerator Fall()
+	{
+		Debug.Log("Fall Start: " + Time.time + " | " + (fallTime - totalBlinkTime_) + " | " + (totalBlinkTime_ - fallTime));
+		
+		yield return new WaitForSeconds( fallTime - totalBlinkTime_ );
+		Debug.Log("Fall End: " + Time.time);
+		
+		int blinkCount = (int)( totalBlinkTime_ / blinkTime);
+		for( int blinkIndex = 0;blinkIndex < blinkCount; blinkIndex++)
+		{
+			this.renderer.enabled = !this.renderer.enabled;//I love this :)
+			yield return new WaitForSeconds(blinkTime);
+		}
+		
+		this.renderer.enabled = true;
+		Rigidbody2D rigidBody = this.gameObject.AddComponent<Rigidbody2D>();
+		this.transform.rigidbody2D.gravityScale = 1;
+		this.transform.rigidbody2D.mass = 10;
+	}	
+	
+	IEnumerator FadeOut()
+	{
+		int blinkCount = (int)(totalBlinkTime_ / blinkTime);
+		for( int blinkIndex = 0;blinkIndex < blinkCount; blinkIndex++)
+		{
+			this.renderer.enabled = !this.renderer.enabled;
+			yield return new WaitForSeconds(blinkTime / 2);
+		}
+		
+		Destroy(this.gameObject);
+	}	
 	
 }
