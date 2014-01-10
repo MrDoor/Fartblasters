df12253aceeec0aa535fc9fee16a5de858edd9c7
nakedeskimosuperhero <nakedeskimosuperhero@gmail.com>
2014-01-10 14:44:37 -0800

    Added breakable block

---
+++ Assets/Scripts/BreakBlockCollision.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakBlockCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /*oid OnCollisionEnter2D( Collision2D coll )
+    {
+    	
+        if( coll.gameObject.tag.Equals( "Player" ) )
+        {
+            this.renderer.enabled = false;
+            this.gameObject.collider2D.enabled = false;
+        }
+    }*/
+	
+
+    void OnTriggerEnter2D( Collider2D obj )
+    {
+        if (obj.name == "Player")
+        {
+			PlayerControl pControl = obj.GetComponent<PlayerControl>();
+			if( pControl )
+			{
+				this.transform.renderer.enabled = false;
+				this.gameObject.collider2D.enabled = false;
+				pControl.transform.rigidbody2D.AddForce( pControl.transform.rigidbody2D.velocity.normalized * -3000 );
+			}
+        }
+    }
+}
