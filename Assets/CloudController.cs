0c6974cbf3460952f38305909941092387e479d8
zackLore <zacktron@gmail.com>
2014-06-01 22:02:33 -0700

    More touchups on levels.  Added clouds.  Need more/better clouds.

---
+++ Assets/CloudController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour {
+
+	public bool goRight;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		speed = speed / 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( goRight )
+		{
+			Vector2 temp = this.transform.position;
+			temp.x += speed;
+			this.transform.position = temp;
+		}
+	}
+}
