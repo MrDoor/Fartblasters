92f5c5e5d2442b451c3986d18c78e097b57c0110
zackLore <zacktron@gmail.com>
2014-06-09 18:17:54 -0700

    ... redo...

--- Assets/Scripts/SpawnItem.cs
+++ Assets/Scripts/SpawnItem.cs
@@ -3,13 +3,64 @@ using System.Collections;
 
 public class SpawnItem : MonoBehaviour {
 
+	public GameObject item;
+	public GameObject spawnPoint;
+	public bool isMulti;
+	public int spawnCount;
+	public float delay;
+	
+	private float accumulatedDelay;
+	private bool spawned;
 	// Use this for initialization
 	void Start () {
-	
+		accumulatedDelay = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
 	
+	void OnTriggerEnter2D ( Collider2D obj )
+	{
+		if ( item && spawnPoint )
+		{
+			if ( obj.gameObject.name.Equals( "Player" ) )
+			{
+				if( !spawned )
+				{
+					if(!isMulti)
+					{
+						spawned = true;
+						accumulatedDelay = delay;
+						StartCoroutine ( "SpawnItems" );				
+					}
+					else
+					{
+						for( int i = 0; i < spawnCount; i++)
+						{
+							StartCoroutine( "SpawnItems_Random" );
+							accumulatedDelay += delay;
+						}
+						spawned = true;
+					}
+				}
+			}
+		}
+	}
+	
+	IEnumerator SpawnItems_Random()
+	{	
+		yield return new WaitForSeconds( accumulatedDelay );
+		GameObject newItem = (GameObject)Instantiate( item, spawnPoint.transform.position, Quaternion.identity );
+		float x = Random.Range(-200, 200);
+		float y = Random.Range(-200, 200);
+		newItem.transform.rigidbody2D.AddForce( new Vector2( x, y ) );
+	}
+	
+	IEnumerator SpawnItems()
+	{	
+		yield return new WaitForSeconds( accumulatedDelay );
+		GameObject newItem = (GameObject)Instantiate( item, spawnPoint.transform.position, Quaternion.identity );
 	}
 }
