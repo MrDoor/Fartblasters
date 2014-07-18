fbec1d68fa24389e84bd63d369c2fde5534f5891
nakedeskimosuperhero <nakedeskimosuperhero@gmail.com>
2014-07-17 19:41:34 -0700

    no message

---
+++ Assets/Scripts/DemoEndMenu.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoEndMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI () {
+		
+		GUIStyle textStyle = new GUIStyle();
+		textStyle.fontSize = 25;
+		textStyle.fontStyle = FontStyle.BoldAndItalic;
+		textStyle.normal.textColor = Color.blue;
+		
+		float top = Screen.height / 2 + 140;
+		GUI.Label(new Rect(450, top-60, 500,50), "\t\t\t\t\tThank you for playing the FARTBLASTERS! demo.\nLook for the full version comming soon to an app store near you!", textStyle);
+		GUI.Box (new Rect (650, top+20, 360, 200),"");
+		
+		if (GUI.Button (new Rect (670, top+40, 100, 50), "Level 1")) {
+			
+			Debug.Log ("Load Level: " + Application.loadedLevelName);
+			Application.LoadLevel(3);
+		}
+		
+		if(GUI.Button (new Rect(780, top + 40, 100,50), "Level 2"))
+			
+		{
+			Debug.Log ("Load Level: " + Application.loadedLevelName);
+			Application.LoadLevel (4);
+		}
+		
+		if(GUI.Button (new Rect(890, top + 40, 100,50), "Level 3"))
+			
+		{
+			Debug.Log ("Load Level: " + Application.loadedLevelName);
+			Application.LoadLevel (5);
+		}
+
+		if(GUI.Button (new Rect(670, top + 100, 100,50), "Level 4"))
+			
+		{
+			Debug.Log ("Load Level: " + Application.loadedLevelName);
+			Application.LoadLevel (6);
+		}
+
+		if(GUI.Button (new Rect(780, top + 100, 100,50), "Level 5"))
+			
+		{
+			Debug.Log ("Load Level: " + Application.loadedLevelName);
+			Application.LoadLevel (7);
+		}
+
+		if(GUI.Button (new Rect(890, top + 100, 100,50), "Level 6"))
+			
+		{
+			Debug.Log ("Load Level: " + Application.loadedLevelName);
+			Application.LoadLevel (8);
+		}
+
+		if(GUI.Button (new Rect(780, top + 160, 100,50), "Return to \nMain Menu"))
+			
+		{
+			Debug.Log ("Load Level: " + Application.loadedLevelName);
+			Application.LoadLevel (0);
+		}
+
+
+
+	}
+}
---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+	public Texture2D btn_pause;
+	// Use this for initialization
+	private float boxSize = 100f;
+	private Rect backgroundRect;
+	private bool paused = false;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnMouseUpAsButton()
+	{
+		Pause ();
+	}
+	
+
+	public void Pause()
+	{
+		if (paused) 
+		{
+			paused = false;
+			Time.timeScale = 1;
+		}
+		else
+		{
+			paused = true;
+			Time.timeScale = 0;
+		}
+	}
+
+	void OnGUI()
+	{
+		float left		= 20;
+		float top		= Screen.height - 80;
+		float width		= boxSize;
+		float height	= boxSize / 2;
+		
+		backgroundRect = new Rect(left, top, width, height);
+		GUI.DrawTexture(backgroundRect, btn_pause);	
+		
+	
+				if (paused) {
+						/*GUIStyle textStyle = new GUIStyle();
+			textStyle.normal.textColor = Color.red;
+			textStyle.fontSize = 80;
+			textStyle.fontStyle = FontStyle.Bold;
+			
+			GUI.Label(new Rect(700,200 ,Screen.width,Screen.height),"Game Over", textStyle);
+			*/
+			
+						//GUI.Label (new Rect (750, 250, 300, 50), "GAME OVER");
+						GUI.Box (new Rect (810, 300, 200, 200), "");
+			
+			
+						if (GUI.Button (new Rect (860, 315, 100, 50), "Restart Level")) {
+								Debug.Log ("Load Level: " + Application.loadedLevelName);
+				
+								//if (GUI.Button (new Rect (720, 320, 100, 50), "Restart Level")) {
+								//Debug.Log ("Load Level: " + Application.loadedLevelName);
+				
+								Application.LoadLevel (Application.loadedLevel);
+								Time.timeScale = 1;
+						}
+			
+						if (GUI.Button (new Rect (860, 375, 100, 50), "Return to \nMain Menu")) {
+								//Debug.Log (Util.getlevel);
+								Application.LoadLevel ("test_menu_Nick");
+								Time.timeScale = 1;
+						}
+			
+						if (GUI.Button (new Rect (860, 435, 100, 50), "Quit Game")) {
+								Application.Quit ();
+						}
+			
+						//GUI.Label (new Rect(850, 500, Screen.width, Screen.height), "You have died " + DBFunctions.getTimesDied () + " Times.");
+				}
+		}
+}
